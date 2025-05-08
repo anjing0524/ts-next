c6ae3c5733588134ac379ae60353bad0965dd9e9
青荼 <10788202+color-tea@user.noreply.gitee.com>
2025-05-08 09:52:22 +0800

    fix: change nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,5 @@
 const nextConfig = {
   output: 'standalone',
-  async headers() {
-    // Define cross-origin isolation headers
-    const crossOriginIsolationHeaders = [
-      { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
-      { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
-      { key: 'Cross-Origin-Resource-Policy', value: 'same-origin' },
-      { key: 'Permissions-Policy', value: 'interest-cohort=()' },
-      { key: 'X-Content-Type-Options', value: 'nosniff' },
-    ];
-
-    return [
-      {
-        source: '/:path*',
-        headers: crossOriginIsolationHeaders,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
